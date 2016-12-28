4fe21a56e774f65dac5e3221509480dafd6a2ba1
jorarsus <jorarsus@gmail.com>
2016-12-28 13:50:10 +0100

    Initial seed data

--- PersonalBudgetWPF/Migrations/Configuration.cs
+++ PersonalBudgetWPF/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace PersonalBudgetWPF.Migrations
 {
+    using EF;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -27,6 +28,39 @@ namespace PersonalBudgetWPF.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            context.Accounts.AddOrUpdate(
+                p => p.Concept,
+                new Account { Concept = "Agua" },
+                new Account { Concept = "Alquiler" },
+                new Account { Concept = "Apoyo doméstico" },
+                new Account { Concept = "Bolsillo" },
+                new Account { Concept = "Caja" },
+                new Account { Concept = "Formación" },
+                new Account { Concept = "Cuenta corriente" },
+                new Account { Concept = "Deudores" },
+                new Account { Concept = "Informática" },
+                new Account { Concept = "Electricidad" },
+                new Account { Concept = "Hogar" },
+                new Account { Concept = "Gas" },
+                new Account { Concept = "Gasolina" },
+                new Account { Concept = "Impuestos" },
+                new Account { Concept = "Ingresos trabajo" },
+                new Account { Concept = "Inversiones financieras" },
+                new Account { Concept = "Libros/juegos" },
+                new Account { Concept = "Mantenimiento casa" },
+                new Account { Concept = "Mantenimiento coche" },
+                new Account { Concept = "Médico/farmacia/personales" },
+                new Account { Concept = "Alimentación" },
+                new Account { Concept = "Otros" },
+                new Account { Concept = "Otros transportes" },
+                new Account { Concept = "Pendiente de asignar" },
+                new Account { Concept = "Regalos/transferencias" },
+                new Account { Concept = "Restaurante/cine/otros" },
+                new Account { Concept = "Ropa" },
+                new Account { Concept = "Internet" },
+                new Account { Concept = "Teléfono" },
+                new Account { Concept = "Viajes/vacaciones" }
+            );
         }
     }
 }
