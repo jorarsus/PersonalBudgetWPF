c3ace27aa234ef35d69a66522c7954c95cf307ab
jorarsus <jorarsus@gmail.com>
2017-01-08 11:17:45 +0100

    Avoid exception when there is not an Account

--- PersonalBudgetWPF/Repos/AccountRepo.cs
+++ PersonalBudgetWPF/Repos/AccountRepo.cs
@@ -35,15 +35,13 @@ namespace PersonalBudgetWPF.Repos
         /// Gets an account with the specified concept
         /// </summary>
         /// <param name="concept">The concept to search</param>
-        /// <returns>Account with the specified concept</returns>
+        /// <returns>Account with the specified concept, or null if no Account found</returns>
         public Account GetAccountByConcept(String concept)
         {
             var query = from account in Context.Accounts
                         where account.Concept == concept
                         select account;
-            return query.First();
+            return query.FirstOrDefault();
         }
-
-        
     }
 }
